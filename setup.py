1d5d25b7b2cddf2af7751da765b7729b43496ea9
miaog001 <bobby.miao@disney.com>
2022-08-29 10:44:21 +0800

    asdf

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pipettes',
+    version='2.0.1',
+    keywords='pipettes',
+    description='a library for piptest Developer',
+    license='MIT License',
+    url='https://github.com/wxnacy/wwx',
+    author='bbb',
+    author_email='miao2005xu@163.com',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+    ],
+)
